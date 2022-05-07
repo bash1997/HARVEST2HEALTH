eb2f48c672770eba2ff6893f2e543cf54a702833
BSH <bash_1997@outlook.com>
2022-05-07 03:51:56 -0400

    button changed copy func not working

--- js/taplink.js
+++ js/taplink.js
@@ -21,26 +21,46 @@ const NewUser = function(name) {
     return userinfo;
 }
 
-const NewLink = function(title) {
+const NewLink = function(title, id) {
     const link = {};
     link.title = title;
+    link.id = id;
     link.url = "";
     link.description = null;
     link.color = null;
+
+    link.copy = () => {
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(link.description);
+      
+        /* Alert the copied text */
+        // alert("Copied the text: " + link.description);
+    }
 
     link.box = (selection) => {
         const linkbtn = document.createElement("DIV");
         linkbtn.classList.add("box");
         linkbtn.classList.add("col-md-8");
 
-        const btnLink = document.createElement("A");
-        btnLink.classList.add("btn-link");
-        btnLink.setAttribute("href", link.url);
-        btnLink.innerHTML = `<div class='btn-link-title'>${link.title}</div>`;
+        if(link.url == ""){
+            var btnLink = document.createElement("DIV");
+            btnLink.classList.add("btn-link");
+            btnLink.innerHTML = `<div class='btn-link-title'>${link.title}</div>`;
+            btnLink.setAttribute("onclick", () => {
+                navigator.clipboard.writeText(link.description);
+                alert("Copied the text: " + link.description);
+            });
+        } else {
+            var btnLink = document.createElement("A");
+            btnLink.classList.add("btn-link");
+            btnLink.setAttribute("href", link.url);
+            btnLink.innerHTML = `<div class='btn-link-title'>${link.title}</div>`;
+        }
 
         if (link.description != null) {
             const subtitle = document.createElement("DIV");
             subtitle.classList.add("btn-link-subtitle");
+            subtitle.setAttribute("id", link.id);
             subtitle.innerHTML = `${link.description}`;
             btnLink.append(subtitle);
         }
@@ -57,12 +77,28 @@ const NewLink = function(title) {
     return link;
 }
 
-const Standout = function(title) {
+const Standout = function(title, id) {
     const link = {};
     link.title = title;
+    link.id = id;
     link.url = "";
     link.description = null;
     link.color = null;
+
+    link.copy = () => {
+        /* Get the text field */
+        var copyText = document.getElementById(link.id);
+      
+        /* Select the text field */
+        link.description.select();
+        // copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      
+         /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
+      
+        /* Alert the copied text */
+        alert("Copied the text: " + copyText.value);
+    }
 
     link.box = (selection) => {
         const linkbtn = document.createElement("DIV");
@@ -75,9 +111,14 @@ const Standout = function(title) {
         btnLink.setAttribute("href", link.url);
         btnLink.innerHTML = `<div class='btn-link-title'>${link.title}</div>`;
 
+        if(link.url == ""){
+            btnLink.setAttribute("onclick", link.copy);
+        }
+
         if (link.description != null) {
             const subtitle = document.createElement("DIV");
             subtitle.classList.add("btn-link-subtitle");
+            subtitle.setAttribute("id", link.id);
             subtitle.innerHTML = `${link.description}`;
             btnLink.append(subtitle);
         }
@@ -108,24 +149,24 @@ container.classList.add("content-wrapper");
 container.classList.add("container");
 siteSection.append(container);
 
-const cashapp = NewLink("Cash App");
+const cashapp = NewLink("Cash App", "cashapp");
 cashapp.url = "https://cash.app/$nucas";
 cashapp.description = "$nucas";
 cashapp.box(container);
 
-const paypal = NewLink("PayPal");
+const paypal = NewLink("PayPal", "paypal");
 paypal.description = "Eugene Yarbrough";
 paypal.box(container);
 
-const venmo = NewLink("Venmo");
+const venmo = NewLink("Venmo", "venmo");
 venmo.description = "Eugene-Yarbrough";
 venmo.box(container);
 
-const zelle = NewLink("Zelle");
+const zelle = NewLink("Zelle", "zelle");
 zelle.description = "19176538743";
 zelle.box(container);
 
-const subscriber = Standout("Subscribe To Our Email List");
+const subscriber = Standout("Subscribe To Our Email List", "subscribe");
 subscriber.url = "http://eepurl.com/h1uYX9";
 subscriber.description = "Get a weekly produce list, and a list of the locations we are currently at.";
 subscriber.box(container)
