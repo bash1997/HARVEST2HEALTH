06c4d67cb25a3f76cf2c5bee2cb298b8a309129c
Bria Saunders-Hall <41771329+bash1997@users.noreply.github.com>
2022-05-07 12:51:38 -0400

    Hide the subscriber link

--- js/taplink.js
+++ js/taplink.js
@@ -169,4 +169,4 @@ zelle.box(container);
 const subscriber = Standout("Subscribe To Our Email List", "subscribe");
 subscriber.url = "http://eepurl.com/h1uYX9";
 subscriber.description = "Get a weekly produce list, and a list of the locations we are currently at.";
-subscriber.box(container)
+// subscriber.box(container)
